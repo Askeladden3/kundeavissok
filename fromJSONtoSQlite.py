37f839dcea337f51f1b8e6673417c96e4cbeaded
Ask <askhf.ntnu.no>
2025-09-22 13:34:11 +0200

    debugging

--- fromJSONtoSQlite.py
+++ fromJSONtoSQlite.py
@@ -14,6 +14,9 @@ def JSONtoSQlite(dato):
     os.mkdir('temp_output/databaser')
     db_file = f'temp_output/databaser/kundeavis_{UKE}_test.db'
 
+    os.listdir('temp_output') #Debugging, fjern senere
+    os.listdir('temp_output/results_JSON')
+
     def EXECUTE_TABLES(item, table_name):
         dato = datetime.strptime(f'{år} {UKE} 1', '%G %V %u').date()
 
