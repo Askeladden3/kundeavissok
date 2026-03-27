52bd1979633002e33f9c0a55161bca71f1a28640
Askeladden3 <askinatr@gmail.com>
2026-03-27 13:27:13 +0100

    Forbedret mySQL-behandling
    -Skal nå fungere med typer samt FTS

--- fromJSONtoSQlite.py
+++ fromJSONtoSQlite.py
@@ -5,74 +5,44 @@ import time
 from datetime import datetime
 import sqlite3
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Integer, String, Float, text
 
 
 def JSONtoSQlite(dato):
 
     curr_date, år, UKE = dato
 
-    try:
-        os.mkdir('temp_output/databaser')
-    except:
-        pass
-    db_file = f'temp_output/databaser/kundeavis_{UKE}.db'
+    default_column_types = {
+    'name': String(355),
+    'brand': String(100),
+    'total_mass': Float(),
+    'unit': String(30),
+    'store': String(30),
+    'brand': String(50),
+    'category': String(),
+    'protein_type': String(),
+    'AI_model_used': String(),
+    'page_number': Integer()
+    }
 
-    default_table_args = '''
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    total_mass DECIMAL (8,3),
-    unit TEXT DEFAULT 'kg',
-    store TEXT NOT NULL,
-    brand TEXT,
-    category TEXT,
-    protein_type TEXT,
-    AI_model_used TEXT,
-    page_number INTEGER
-    '''
-    
-    def CREATE_TABLE(table_name):
-        tableDict = {
-            'percentage_deal' : f'''
-            CREATE TABLE IF NOT EXISTS {table_name} (
-                {default_table_args},
-                percentage_off INTEGER NOT NULL
-                )''',
+    percentage_deal_types = {
+        'percentage_off': Integer()
+    } | default_column_types
 
-            'standard_deal' : f'''
-            CREATE TABLE IF NOT EXISTS {table_name} (
-                {default_table_args},
-                price_per_unit DECIMAL (10,2),
-                total_price DECIMAL (10,2)
-                )''',
-            'bogo_deal' : f'''
-            CREATE TABLE IF NOT EXISTS {table_name} (
-                {default_table_args},
-                items_received INTEGER,
-                items_paid_for INTEGER
-                )'''}
-        
-        return tableDict[table_name]
-    
-    def CREATE_SEARCH_TABLE(table_name, conn):
-        SQdict = {'standard_deal':              ["CREATE VIRTUAL TABLE IF NOT EXISTS standard_deal_fts USING fts5(name, content='standard_deal', content_rowid='id');",
-                                                "INSERT INTO standard_deal_fts(rowid, name) SELECT id, name FROM standard_deal;",],
+    standard_deal_types = {
+        'price_per_unit': Float()
+    } | default_column_types
 
-                    'percentage_deal':          ["CREATE VIRTUAL TABLE IF NOT EXISTS percentage_deal_fts USING fts5(name, content='percentage_deal', content_rowid='id');",
-                                                "INSERT INTO percentage_deal_fts(rowid, name) SELECT id, name FROM percentage_deal;",],
+    bogo_deal_types = {
+        'items_recieved': Integer(),
+        'items_paid_for': Integer()
+    } | default_column_types
 
-
-                    'bogo_deal':                ["CREATE VIRTUAL TABLE IF NOT EXISTS bogo_deal_fts USING fts5(name, content='bogo_deal', content_rowid='id');",
-                                                "INSERT INTO bogo_deal_fts(rowid, name) SELECT id, name FROM bogo_deal;",],
-
-                    
-                                               }
-        SQcommands = SQdict[table_name]
-
-        for command in SQcommands:
-            print(command)
-            conn.execute(command)
-
+    db_types = {
+        'standard_deal' : standard_deal_types,
+        'percentage_deal': percentage_deal_types,
+        'bogo_deal' : bogo_deal_types
+    }
 
     protein_dict = {None:None, "beef" : "Ku", "pork" : "Svin", "poultry": "Kylling", "lamb" : "Lam", "mixed": "Blandet", "plant_based": "Plante-basert"}
     JSON_dirpath = f'temp_output/results_JSON'
@@ -98,14 +68,17 @@ def JSONtoSQlite(dato):
             data.to_sql(name=table_name,
                     con=engine,
                     index=False,
-                    if_exists='replace')
+                    if_exists='replace',
+                    dtype=db_types[table_name])
+            
+            with engine.connect() as conn:
+                conn.execute(text(f"ALTER TABLE {table_name} ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY FIRST;"))
+                conn.execute(text(f"ALTER TABLE {table_name} ADD FULLTEXT(name);"))
+                conn.commit()
             
             stores = set(data['store'])
             unique_stores = unique_stores.union(stores)
             
-            with engine.connect() as conn:
-                CREATE_SEARCH_TABLE(table_name, conn)
-                conn.commit()
 
     return unique_stores
 
