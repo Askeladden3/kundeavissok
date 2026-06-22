201f82d8e7b24f8a809993e0c42042a973c4bcc7
Askeladden3 <askinatr@gmail.com>
2026-06-22 21:25:59 +0200

    Removed unutilized exe-file, and specified exact path to downloaded chromedriver

--- kundeavis_fetch.py
+++ kundeavis_fetch.py
@@ -118,13 +118,13 @@ def fetch_etilbudsavis(BUTIKKER, dato):
     page_struct = []
     API_KEY = os.environ["GEMINI_API_KEY"]
 
-    service = Service(ChromeDriverManager().install())
+    service = Service(executable_path="/usr/bin/chromedriver")
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")                # Required: Bypasses OS security model inside Docker
     options.add_argument("--disable-dev-shm-usage")     # Required: Prevents Docker from running out of memory
     options.add_argument("--disable-gpu")
-
+    options.binary_location = "/usr/bin/chromium"
 
     for helgetilbud in BUTIKKER:
         count = 0
