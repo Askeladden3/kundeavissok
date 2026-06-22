0a6485c0126a890febace7d2ff0c86d43c59921f
Askeladden3 <askinatr@gmail.com>
2026-06-22 22:38:04 +0200

    Init genai-client once instead of per try
    Also changed front_page_model because of consistent issues with high demand for previous choice

--- AI_model_setup.py
+++ AI_model_setup.py
@@ -25,4 +25,4 @@ Gem31_flash_lite = API_model('gemini-3.1-flash-lite', 'Gemini 3.1 Flash Lite', 1
 #Gem_pro = API_model('pro', 'Gemini Pro', 2, 50)
 
 AI_models = [Gem35_flash, Gem3_flash, Gem31_flash_lite, Gem25_flash]
-front_page_model = Gem3_flash
+front_page_model = Gem35_flash
